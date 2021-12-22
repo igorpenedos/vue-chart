161b0b0a58ba5116b0a07752a547af3bb13379e5
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2021-12-22 14:34:22 -0500

    [CHORE]: Setup Package

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id='app'>
+    <p>This is a test, so everything seems to be OK, GOOD LUCK FUTURE DEVELOPER</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
